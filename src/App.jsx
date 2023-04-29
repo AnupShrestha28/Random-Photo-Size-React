5c9d8f2d7da45090cd77e3611576fd2bbdf3b7e5
AnupShrestha28 <anupstha0168@gmail.com>
2023-04-29 13:20:55 +0545

    Done

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+function App() {
+  const [photos, setPhotos] = useState([]);
+  const [currentPhoto, setCurrentPhoto] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleClick = () => {
+    fetch('https://jsonplaceholder.typicode.com/photos')
+      .then(response => response.json())
+      .then(data => setPhotos(data))
+      .catch(error => console.log(error));
+  };
+
+  useEffect(() => {
+    if (photos.length > 0) {
+      setCurrentPhoto(photos[currentIndex]);
+    }
+  }, [photos, currentIndex]);
+
+  const handleNext = () => {
+    setCurrentIndex(currentIndex + 1);
+  };
+
+  const handlePrevious = () => {
+    setCurrentIndex(currentIndex - 1);
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}>Click Me</button>
+      {currentPhoto && (
+        <div key={currentPhoto.id}>
+          <h3>{currentPhoto.title}</h3>
+          <img src={currentPhoto.url} alt={currentPhoto.title} />
+          {currentIndex > 0 && (
+            <button onClick={handlePrevious}>Previous</button>
+          )}
+          {currentIndex < photos.length - 1 && (
+            <button onClick={handleNext}>Next</button>
+          )}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
