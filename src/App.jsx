d0498aad5629da555a38817e28d3d9c1caaeb0d2
AnupShrestha28 <anupstha0168@gmail.com>
2023-04-30 09:58:05 +0545

    Local Storage in functional Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,26 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react'
 
-const BASE_URL = 'https://jsonplaceholder.typicode.com/photos';
+export default function App() {
 
-function App() {
-  const [photos, setPhotos] = useState([]);
-  const [currentPhoto, setCurrentPhoto] = useState(null);
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [data, setData] = useState("");
 
-  const handleClick = () => {
-    fetch(BASE_URL)
-      .then(response => response.json())
-      .then(data => setPhotos(data))
-      // .catch(error => console.log(error));
-  };
+ useEffect(() => {
+  const storedData = localStorage.getItem("myData");
+  if(storedData){
+    setData(storedData);
+  }
+ }, [])
 
-  useEffect(() => {
-    if (photos.length > 0) {
-      setCurrentPhoto(photos[currentIndex]);
-    }
-  }, [photos, currentIndex]);
-
-  const handleNext = () => {
-    setCurrentIndex(currentIndex + 1);
-  };
-
-  const handlePrevious = () => {
-    setCurrentIndex(currentIndex - 1);
-  };
+ const handleChange = (e) =>{
+  const newData = e.target.value;
+  setData(newData);
+  localStorage.setItem("myData", newData);
+ }
 
   return (
     <div>
-      <button onClick={handleClick}>Click Me</button>
-      {currentPhoto && (
-        <div key={currentPhoto.id}>
-          <h3>{currentPhoto.title}</h3>
-          <img src={currentPhoto.url} alt={currentPhoto.title} />
-          {currentIndex > 0 && (
-            <button onClick={handlePrevious}>Previous</button>
-          )}
-          {currentIndex < photos.length - 1 && (
-            <button onClick={handleNext}>Next</button>
-          )}
-        </div>
-      )}
+      <input type='text' placeholder='Enter Something here' value={data} onChange={handleChange} />
+      <p>Saved Data : {data}</p>
     </div>
-  );
+  )
 }
-
-export default App;
