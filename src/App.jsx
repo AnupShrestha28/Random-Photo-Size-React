a290eba867134c60b89487228e6ab10bf58cb08e
AnupShrestha28 <anupstha0168@gmail.com>
2023-04-30 10:28:18 +0545

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,53 @@
 import React, { useEffect, useState } from 'react'
 
+const BASE_URL = 'https://jsonplaceholder.typicode.com/photos';
+
 export default function App() {
 
-  const [data, setData] = useState("");
+  const [photos, setPhotos] = useState([]);
+  const [currentPhoto, setCurrentPhoto] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
- useEffect(() => {
-  const storedData = localStorage.getItem("myData");
-  if(storedData){
-    setData(storedData);
+  useEffect(() => {
+    if(photos.length > 0){
+      setCurrentPhoto(photos[currentIndex])
+    }
+  }, [photos, currentIndex]);
+  
+  const handleChange = () => {
+    fetch(BASE_URL)
+      .then(res => res.json())
+      .then(data => setPhotos(data))
+      .catch(err => console.log(err))
+
   }
- }, [])
 
- const handleChange = (e) =>{
-  const newData = e.target.value;
-  setData(newData);
-  localStorage.setItem("myData", newData);
- }
+
+  const handlePrevious = () => {
+    setCurrentIndex(currentIndex - 1);
+  }
+
+  const handleNext = () => {
+    setCurrentIndex(currentIndex + 1);
+  }
 
   return (
     <div>
-      <input type='text' placeholder='Enter Something here' value={data} onChange={handleChange} />
-      <p>Saved Data : {data}</p>
+      <button onClick={handleChange}>Click Me</button>
+      {currentPhoto && (
+        <div key={currentPhoto.id}>
+          <h3>{currentPhoto.title}</h3>
+          <img src={currentPhoto.url} />
+        </div>
+      )}
+
+      {currentIndex > 0 && (
+        <button onClick={handlePrevious}>Previous</button>
+      )}
+
+      {currentIndex < photos.length - 1 && (
+        <button onClick={handleNext}>Next</button>
+      )}
     </div>
   )
 }
